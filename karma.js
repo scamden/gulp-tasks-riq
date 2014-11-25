3a4aac8f02a9f9f2180bca353acc18172f7eb3dd
Sterling Camden <sterling@relateiq.com>
2014-11-24 16:47:32 -0800

    allow generic configure for karma conf

--- karma.js
+++ karma.js
@@ -22,6 +22,9 @@ module.exports = function (taskOpts) {
                     opts.preprocessors[glob] = ['browserify'];
                 });
             }
+            if (taskOpts.configure) {
+                taskOpts.configure(karmaConf);
+            }
         }
     });
 
