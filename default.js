c79e187cee1aaa47160c675d7871921fc8e52133
Sterling Camden <sterling@relateiq.com>
2014-12-01 12:17:32 -0800

    pass in karma conf instead of reading it

--- default.js
+++ default.js
@@ -24,7 +24,7 @@ module.exports = function (opts) {
         runSequence(
             'clean',
             'prebuild',
-            'watchify-omega',
+            'browserify-omega',
             'watchServe',
             cb
         );
--- karma.js
+++ karma.js
@@ -2,45 +2,40 @@ var gulp = require('gulp');
 var karma = require('gulp-karma');
 
 module.exports = function (taskOpts) {
-    var karmaConf;
-    require(taskOpts.karmaConfPath || 'karma.conf.js')({
-        set: function (opts) {
-            karmaConf = opts;
-            opts.browserify = {
-                cache: {}, packageCache: {}, fullPaths: true, debug: true
-            };
-            opts.browserify.transform = taskOpts.transforms || [];
-            if (taskOpts.coverage) {
-                opts.browserify.transform.push('browserify-istanbul');
-            }
+    var karmaConf = taskOpts.karmaConf;
+    karmaConf.browserify = {
+        cache: {}, packageCache: {}, fullPaths: true, debug: true
+    };
+    karmaConf.browserify.transform = taskOpts.transforms || [];
+    if (taskOpts.coverage) {
+        karmaConf.browserify.transform.push('browserify-istanbul');
+    }
 
-            opts.files = opts.files || [];
-            opts.preprocessors = opts.preprocessors || {};
-            if (taskOpts.testGlobs) {
-                taskOpts.testGlobs.forEach(function (glob) {
-                    opts.files.push(glob);
-                    opts.preprocessors[glob] = ['browserify'];
-                });
-            }
-            if (taskOpts.configure) {
-                taskOpts.configure(karmaConf);
-            }
-        }
-    });
+    karmaConf.files = karmaConf.files || [];
+    karmaConf.preprocessors = karmaConf.preprocessors || {};
+    if (taskOpts.testGlobs) {
+        taskOpts.testGlobs.forEach(function (glob) {
+            karmaConf.files.push(glob);
+            karmaConf.preprocessors[glob] = ['browserify'];
+        });
+    }
+    if (taskOpts.configure) {
+        taskOpts.configure(karmaConf);
+    }
 
-    function setupKarmaOpts(action) {
+    function setKarmaOptsAction(action) {
         karmaConf.action = action;
         return karmaConf;
     }
 
     gulp.task('karma-watch', function () {
         return gulp.src(karmaConf.files)
-            .pipe(karma(setupKarmaOpts('watch')));
+            .pipe(karma(setKarmaOptsAction('watch')));
     });
 
     //arbitrarily pick karma as the export
     return gulp.task('karma', function () {
         return gulp.src(karmaConf.files)
-            .pipe(karma(setupKarmaOpts('run')));
+            .pipe(karma(setKarmaOptsAction('run')));
     });
 };
